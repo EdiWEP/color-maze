8edbab93af124b3ba05112d1b7539a44cb4df5ee
Waren <edipodaru@gmail.com>
2018-07-17 17:16:25 +0300

    Added Teleporters

---
+++ Assets/Scripts/TeleportPlayer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ColorEnum;
+
+public class TeleportPlayer : MonoBehaviour {
+
+    public GameObject otherTeleporter;
+    public int numberOfTeleporters;
+
+    private ColorX teleporterColor;
+    private static float delay;
+    private Vector3 otherTeleporterPosition;
+    
+    void Awake ()
+    {
+        delay = 2.5f;
+    }
+
+	void Start () {
+
+        teleporterColor = GetComponent<TeleporterColor>().color;
+        otherTeleporterPosition = otherTeleporter.transform.position;
+        otherTeleporterPosition.y += 0.7f;
+    }
+	
+	void OnTriggerEnter(Collider col)
+    {
+        if(col.CompareTag("Player") && delay == 2.5f)
+        {
+            if (col.GetComponent<PlayerMovement>().playerColor == teleporterColor)
+            {
+                delay = 0.0f;
+                col.transform.position = otherTeleporterPosition;
+            }
+        }
+    }
+
+    void Update ()
+    {
+        if (delay < 2.5f)
+        {
+            delay += (Time.deltaTime / numberOfTeleporters);
+        }
+        if (delay > 2.5f)
+        {
+            delay = 2.5f;
+        }
+    }
+}
---
+++ Assets/Scripts/TeleporterColor.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ColorEnum;
+
+
+public class TeleporterColor : MonoBehaviour {
+
+    public ColorX color;
+
+    private Material teleporterMaterial;
+	
+	void Start () {
+
+        teleporterMaterial = GetComponent<Renderer>().material;
+        switch (color)
+        {
+            case ColorX.red:
+                teleporterMaterial.SetColor("_Color", Color.red);
+                break;
+
+            case ColorX.green:
+                teleporterMaterial.SetColor("_Color", Color.green);
+                break;
+
+            case ColorX.blue:
+                teleporterMaterial.SetColor("_Color", Color.blue);
+                break;
+
+            case ColorX.pink:
+                teleporterMaterial.SetColor("_Color", Color.magenta);
+                break;
+
+            case ColorX.yellow:
+                teleporterMaterial.SetColor("_Color", Color.yellow);
+                break;
+
+            case ColorX.cyan:
+                teleporterMaterial.SetColor("_Color", Color.cyan);
+                break;
+            default:
+                break;
+        }
+    }
+	
+	
+}
